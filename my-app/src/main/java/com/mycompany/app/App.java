0f788b3d2414cda0c6ad911f9c07f33d2d21f49f
Dmitriy Drinfeld <dmitriy.drinfeld@dimasmac.local>
2018-04-16 16:20:58 -0400

    Small change to trigger build

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-
+        //TEST
         System.out.println("A NullPointerException is a drag...");
         Object myObject = null;
         /**
